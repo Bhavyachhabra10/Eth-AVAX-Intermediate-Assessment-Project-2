b28027410a00c00412074b69af85a60783829850
Bhavyachhabra10 <bhavyachhabra9896@gmail.com>
2024-06-30 22:23:39 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,157 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+
+export default function HomePage() {
+  const [ethWallet, setEthWallet] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
+  const [atm, setATM] = useState(undefined);
+  const [balance, setBalance] = useState(undefined);
+  const [owner, setOwner] = useState();
+  const [contractBal, setContractBal] = useState();
+  const [IsOwner, setIsOwner] = useState();
+
+  const contractAddress = "0x945834790C78067fFEA9198d842Ff32e9B203A7e";
+  const atmABI = atm_abi.abi;
+
+  const getWallet = async () => {
+    if (window.ethereum) {
+      setEthWallet(window.ethereum);
+    }
+
+    if (ethWallet) {
+      const account = await ethWallet.request({ method: "eth_accounts" });
+      handleAccount(account);
+    }
+  };
+
+  const handleAccount = (account) => {
+    if (account) {
+      console.log("Account connected: ", account);
+      setAccount(account);
+    } else {
+      console.log("No account found");
+    }
+  };
+
+  const connectAccount = async () => {
+    if (!ethWallet) {
+      alert("MetaMask wallet is required to connect");
+      return;
+    }
+
+    const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
+    handleAccount(accounts);
+
+    // once wallet is set we can get a reference to our deployed contract
+    getATMContract();
+  };
+
+  const getATMContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethWallet);
+    const signer = provider.getSigner();
+    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
+
+    setATM(atmContract);
+  };
+
+  const getBalance = async () => {
+    if (atm) {
+
+      const bal = await atm.getBalance();
+      setBalance(parseInt(bal))
+      // setBalance((await atm.getBalance()).toNumber());
+    }
+  };
+
+  const deposit = async () => {
+    if (atm) {
+      let tx = await atm.deposit(1);
+      await tx.wait();
+      getBalance();
+    }
+  };
+
+  const getOwnerInfo = async () => {
+    if (atm) {
+      const owner = await atm.getOwner();
+      setOwner(owner);
+    }
+  };
+  const getContractBalance = async () => {
+    if (atm) {
+      const contractBalance = await atm.getContractBalance();
+      setContractBal(parseInt(contractBalance));
+    }
+  };
+
+  const checkForOwner = async () => {
+    if (atm) {
+      const res = await atm.isOwner();
+      setIsOwner(res);
+    }
+  };
+
+  const withdraw = async () => {
+    if (atm) {
+      let tx = await atm.withdraw(1);
+      await tx.wait();
+      getBalance();
+    }
+  };
+
+  const initUser = () => {
+    // Check to see if user has Metamask
+    if (!ethWallet) {
+      return <p>Please install Metamask in order to use this ATM.</p>;
+    }
+
+    // Check to see if user is connected. If not, connect to their account
+    if (!account) {
+      return (
+        <button onClick={connectAccount}>
+          Please connect your Metamask wallet
+        </button>
+      );
+    }
+
+    if (balance == undefined) {
+      getBalance();
+    }
+
+    return (
+      <div>
+        <p>Your Account: {account}</p>
+        <p>Your Balance: {balance}</p>
+        <button onClick={deposit}>Deposit 1 ETH</button>
+        <button onClick={withdraw}>Withdraw 1 ETH</button>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    getWallet();
+  }, []);
+
+  return (
+    <main className="container">
+      <header>
+        <h1>Welcome to the Metacrafters ATM!</h1>
+      </header>
+      {initUser()}
+      <style jsx>
+        {`
+          .container {
+            text-align: center;
+          }
+        `}
+      </style>
+      <button onClick={getOwnerInfo}>Get Owner Info</button>
+      <button onClick={getContractBalance}> Get Contract Balance</button>
+      <button onClick={checkForOwner}>Check Owner</button>
+      {owner}
+      {contractBal}
+      {IsOwner ? "You are Indeed the owner" : "You are not the owner"}
+    </main>
+  );
+}
